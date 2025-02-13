94111ee52c44ed0ec78dda812ecd842d6cf465c6
p4velstar <pavelmarina976@gmail.com>
2025-02-13 23:57:58 +0200

    Created a header for the site, as well as added a background for the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
-import Header from "./assets/Components/Header/Header.jsx";
-import HomeMain from "./assets/Components/HomeMain/HomeMain.jsx";
+import Header from "./Components/Header/Header.jsx";
+import HomeMain from "./Components/HomeMain/HomeMain.jsx";
 
 export default function App() {
   return (
